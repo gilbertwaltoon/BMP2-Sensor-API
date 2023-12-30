b7a1a13d82962fd201a061182928691b97e3790e
gilbertwaltoon <gilbertwaltoon@gmail.com>
2023-12-30 18:22:50 +0000

    Changes to bmp280_xtras

--- bmp280_xtras.c
+++ bmp280_xtras.c
@@ -15,24 +15,31 @@ static const char *TAG = "bmp280_xtras.c";
  */
 esp_err_t bmp280_init(struct bmp2_dev *dev){
     int8_t rslt;
-    rslt = bmp2_init(&dev);
+    rslt = bmp2_init(dev);
 
     switch (rslt)
     {
     case BMP2_E_NULL_PTR:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_NULL_PTR");
+        break;
     case BMP2_E_COM_FAIL:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_COM_FAIL");
+        break;
     case BMP2_E_INVALID_LEN:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_INVALID_LEN");
+        break;
     case BMP2_E_DEV_NOT_FOUND:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_DEV_NOT_FOUND");
+        break;
     case BMP2_E_UNCOMP_TEMP_RANGE:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_UNCOMP_TEMP_RANGE");
+        break;
     case BMP2_E_UNCOMP_PRESS_RANGE:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_UNCOMP_PRESS_RANGE");
+        break;
     case BMP2_E_UNCOMP_TEMP_AND_PRESS_RANGE:
         ESP_RETURN_ON_ERROR(ESP_FAIL, TAG, "BMP2_E_UNCOMP_TEMP_AND_PRESS_RANGE");
+        break;
     default:
         break;
     }
